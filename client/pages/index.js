948dcb9ed3a45339f259c76da5fb76a584acbae7
Huyen Vu <huyenvungoc17@gmail.com>
2022-12-03 23:16:31 +0700

    setup client

---
+++ client/pages/index.js
@@ -0,0 +1,66 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Header } from "../components/Header";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Nhan Nguyen - Construction Documents Management</title>
+        <meta name="description" content="Construction Documents Management" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main className={styles.main}>
+        <h1 className={styles.title}>Quản lý hồ sơ xây dựng</h1>
+
+        <div className={styles.description}>Danh Sách</div>
+        <div class="flex flex-col">
+          <div></div>
+        </div>
+        {/* <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className={styles.card}
+          >
+            <h2>Examples &rarr;</h2>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.card}
+          >
+            <h2>Deploy &rarr;</h2>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div> */}
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Nhan Nguyen 2022
+        </a>
+      </footer>
+    </div>
+  );
+}
---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js, jsx, tsx}", "./component/**/*.{js, jsx, tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
