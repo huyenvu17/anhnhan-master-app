88e25f903947017835c000adf84cc70154c63e96
Huyen Vu <huyenvungoc17@gmail.com>
2022-12-09 16:57:34 +0700

    fetch content list + detail

---
+++ client/pages/[slug].js
@@ -0,0 +1,35 @@
+import { useRouter } from "next/router"
+import React, { useEffect } from "react"
+
+export default function DocumentDetail({ documentInfo }) {
+  const {
+    query: { id },
+  } = useRouter()
+  return (
+    <div className="p-5">
+      {documentInfo?.data?.map((document) => {
+        const { name, content } = document?.attributes
+        return (
+          <div>
+            <p className="text-2xl font-semibold">{name}</p>
+            <div>{content}</div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  console.log("params", params)
+  const { slug } = params
+  const response = await fetch(
+    `${process.env.PUBLIC_URL}/documents?filters[slug]=${slug}`,
+  )
+  const document = await response.json()
+  return {
+    props: {
+      documentInfo: document,
+    },
+  }
+}
--- client/pages/index.js
+++ client/pages/index.js
@@ -4,97 +4,71 @@ import Link from "next/link"
 import { Header } from "../components/Header"
 import styles from "../styles/Home.module.css"
 
-const data = [
-  {
-    name: "Khởi Sự Kinh Doanh",
-    url: "khoi-su-kinh-doanh",
-  },
-  {
-    name: "Văn bản kinh doanh",
-    url: "van-ban-kinh-doanh",
-  },
-  {
-    name: "Văn bản đất đai 1",
-    url: "van-ban-dat-dai-1",
-  },
-  {
-    name: "Văn bản đất đai 2",
-    url: "van-ban-dat-dai-2",
-  },
-  {
-    name: "Văn bản đất đai 3",
-    url: "van-ban-dat-dai-3",
-  },
-  {
-    name: "Văn bản đất đai 4",
-    url: "van-ban-dat-dai-4",
-  },
-  {
-    name: "Văn bản đất đai 5",
-    url: "van-ban-dat-dai-5",
-  },
-  {
-    name: "Văn bản đất đai 6",
-    url: "van-ban-dat-dai-6",
-  },
-]
+export const Category = {
+  DoanhNghiep: "doanhnghiep",
+  CongDan: "congdan",
+}
+
 export default function Home({ documents }) {
-  console.log("documents", documents)
   return (
     <div className={styles.container}>
       <Head>
-        <title>Nhan Nguyen - Construction Documents Management</title>
+        <title>Nhân Nguyễn - Dự Án Quản Lý Hồ Sơ Xây Dựng 2022</title>
         <meta name="description" content="Construction Documents Management" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>Danh Mục Văn Bản</h1>
+        <p className="text-4xl font-semibold text-center mt-10">
+          Danh Mục Văn Bản
+        </p>
 
-        <div className={styles.description}>List</div>
-        <div className="flex flex-col">
-          {documents?.data?.map(({ attributes, id }) => (
-            <Link
-              key={id}
-              href={attributes?.slug || ""}
-              className="p-2 max-w-sm mx-auto border border-orange-300 hover:bg-orange-200 rounded-md shadow-lg space-x-4"
-            >
-              <span>&rarr;</span>
-              <span>{attributes?.name}</span>
-            </Link>
-          ))}
-        </div>
-        {/* <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+        <div className="grid grid-cols-2 gap-5">
+          <div className="">
+            <p className="text-2xl font-semibold text-center mt-10">
+              Doanh Nghiệp
             </p>
-          </a>
-        </div> */}
+            <div className="flex flex-col">
+              {documents?.data
+                ?.filter(
+                  (document) =>
+                    document?.attributes?.category?.data?.attributes?.name ===
+                    Category.DoanhNghiep,
+                )
+                ?.map(({ attributes, id }) => (
+                  <Link
+                    key={id}
+                    href={attributes?.slug || ""}
+                    className="w-2/4 p-2 mx-auto border border-orange-300 hover:bg-orange-200 rounded-md shadow-lg space-x-4"
+                  >
+                    <span>&rarr;</span>
+                    <span>{attributes?.name}</span>
+                  </Link>
+                ))}
+            </div>
+          </div>
+          <div>
+            <p className="text-2xl font-semibold text-center mt-10">Công Dân</p>
+            <div className="flex flex-col">
+              {documents?.data
+                ?.filter(
+                  (document) =>
+                    document?.attributes?.category?.data?.attributes?.name ===
+                    Category.CongDan,
+                )
+                ?.map(({ attributes, id }) => (
+                  <Link
+                    key={id}
+                    href={attributes?.slug || ""}
+                    className="w-2/4 p-2 mx-auto border border-orange-300 hover:bg-orange-200 rounded-md shadow-lg space-x-4"
+                  >
+                    <span>&rarr;</span>
+                    <span>{attributes?.name}</span>
+                  </Link>
+                ))}
+            </div>
+          </div>
+        </div>
       </main>
 
       <footer className={styles.footer}>
@@ -111,7 +85,9 @@ export default function Home({ documents }) {
 }
 
 export async function getServerSideProps() {
-  const response = await fetch(`${process.env.PUBLIC_URL}/documents`)
+  const response = await fetch(
+    `${process.env.PUBLIC_URL}/documents?populate[0]=category`,
+  )
   const data = await response.json()
 
   return {
