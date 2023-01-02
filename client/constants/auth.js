acc5520d312cf2088904b3ff5af5cc5e82425777
Huyen Vu <huyenvungoc17@gmail.com>
2023-01-02 21:14:50 +0700

    create api app for production

--- client/constants/auth.js
+++ client/constants/auth.js
@@ -1,3 +1,5 @@
 export const AUTH_TOKEN = "auth_token"
 export const USER_ITEM = "user_item"
-export const API_HOST = "https://strapi-production-81b5.up.railway.app/api"
+export const API_HOST = "http://localhost:1337/api" // "https://strapi-production-81b5.up.railway.app/api"
+
+//'http://localhost:1337/api'
